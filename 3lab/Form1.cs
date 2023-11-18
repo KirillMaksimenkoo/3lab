f0220ab8451559a2ea13dd0504bfffcbce68961e
Кирило Максименко <maksymenkok@fit.knu.ua>
2023-11-18 19:03:44 +0200

    18.11.2023

--- 3lab/Form1.cs
+++ 3lab/Form1.cs
@@ -24,15 +24,19 @@ namespace _3lab
 
             var n1 = Convert.ToInt32(textBox1.Text);
             var n2 = Convert.ToInt32(textBox2.Text);
+            var a1 = new Mebli[n1];
+            var a2 = new Mebli[n2];
 
             double sum = 0, num = 1;
+            int i2 = 0;
             
             for (int i = 50; i < n1+50; i++)
             {
-                var shafa = new Shafa(i, "Шафа");
-                listBox1.Items.Add(shafa.GetNazva() + " #" + num + ": " + shafa.CCost());
-                sum = sum + shafa.CCost();
+                a1[i2] = new Shafa(i, "Шафа");
+                listBox1.Items.Add(a1[i2].GetNazva() + " #" + num + ": " + a1[i2].CCost());
+                sum = sum + a1[i2].CCost();
                 num++;
+                i2++;
             }
 
             listBox1.Items.Add("Середня ціна шаф: " + sum/n1);
@@ -41,12 +45,13 @@ namespace _3lab
 
             num = 1;
             sum = 0;
+            i2 = 0;
 
             for (int i = 150; i < n2 + 150; i++)
             {
-                var divan = new Divan(i, "Диван");
-                listBox1.Items.Add(divan.GetNazva() + " #" + num + ": " + divan.CCost());
-                sum = sum + divan.CCost();
+                a2[i2] = new Divan(i, "Диван");
+                listBox1.Items.Add(a2[i2].GetNazva() + " #" + num + ": " + a2[i2].CCost());
+                sum = sum + a2[i2].CCost();
                 num++;
             }
 
